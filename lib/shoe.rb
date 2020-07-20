f82bc231aeb39efdf320a6bb24dd40a2964e141a
Timothy Lalli <trlalli92@gmail.com>
2020-07-20 00:21:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,15 +3,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    
+     BRANDS << brand 
   end
   
   BRANDS = []
-
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand 
-  end
     
   def cobble
     self.condition = "new"
