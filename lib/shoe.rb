ade44755043b81d639673a6bcfdda19aaa780395
Timothy Lalli <trlalli92@gmail.com>
2020-07-20 00:21:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,9 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
+    
   end
   
   BRANDS = []
